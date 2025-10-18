f57270d8541b2db2777660d2230977d54fc1f898
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-10-18 13:53:20 -0700

    prepare for future deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv';
 import router from  './routes/'
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 app.use('/', router)
+
+// prepare for deployment
+if (process.env.NODE_ENV === 'production' {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+})
 
 app.listen(PORT, () => {
     console.log(`Listening to port: ${PORT}`)
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,15 @@
+import path from 'path';
 import { Router, Request, Response } from "express";
 import authRouter from "./auth.route";
 import messageRouter from "./messages.route";
-
+const __dirname = path.resolve();
 const router = Router();
 
 router.use('/auth', authRouter)
 router.use('/messages', messageRouter)
+
+router.get('*', (req: Request, res: Response): void => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
+})
 
 export default router;
