efb7efa4f84e4f1e1f331750cc25a923c41ad849
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-10-17 20:52:39 -0700

    messages test route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response } from "express";
 import authRouter from "./auth.route";
+import messageRouter from "./messages.route";
 
 const router = Router();
 
 router.use('/auth', authRouter)
+router.use('/messages', messageRouter)
 
 export default router;
--- backend/src/routes/messages.route.ts
+++ backend/src/routes/messages.route.ts
@@ -3,6 +3,8 @@ import { Router, Request, Response } from 'express'
 const messageRouter = Router();
 
 messageRouter.get('/send', (req: Request, res: Response): void => {
-    console.log('message sent')
-    res.send('Message send'))
-}
+    console.log('message sent!')
+    res.send('message sent')
+})
+
+export default messageRouter
