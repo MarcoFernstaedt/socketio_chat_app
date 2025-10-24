dcc13ab62a3b2dc9ba0197d1bfaa441a97142b6c
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-10-23 20:31:25 -0700

    implament raate limiting and bot detection

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { updateUserProfilePic } from "../controllers/users.controller";
 import { authorizationMiddleware } from "../middleware/auth.middleware";
+import arcjetProtection from "../middleware/arcjet.middleware";
 
 const userRouter = Router();
+
+userRouter.use(arcjetProtection);
 
 userRouter.patch("/me", authorizationMiddleware, updateUserProfilePic);
 
