1510d7c7c85f45eb2bd29c00aa0cd180ea1fa71f
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-11-20 20:44:11 -0700

    correct import arcjet middleware

--- backend/src/middleware/arcjet.middleware.ts
+++ backend/src/middleware/arcjet.middleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import aj from "../lib/arcjet";
+import aj from "../lib/arcjet.js";
 import { isSpoofedBot } from "@arcjet/inspect";
 
 const arcjetProtection = async (
