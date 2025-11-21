cd7918ac1a81561903138f4d377122a0acd52e38
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-11-21 09:58:57 -0700

    ping route for test

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,12 +3,14 @@ import path from 'path';
 import authRouter from './auth.route.js';
 import messageRouter from './messages.route.js';
 import userRouter from './user.route.js';
-import arcjetProtection from '../middleware/arcjet.middleware.js';
+// import arcjetProtection from '../middleware/arcjet.middleware.js';
 
 const router = Router();
-const ROOT = process.cwd(); 
 
-router.use(arcjetProtection);
+// router.use(arcjetProtection);
+router.get('/ping', (req: Request, res: Response) => {
+  console.log('ping')
+})
 
 router.use('/auth', authRouter);
 router.use('/users', userRouter)
