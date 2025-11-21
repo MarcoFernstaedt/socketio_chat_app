ee22a067526ac11d47e238d1e01d37ca15bd1c24
Marco Fernstaedt <fernstaedtmarco@gmail.com>
2025-11-21 10:13:29 -0700

    fix catch all

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,19 +1,20 @@
 import { Router, Request, Response } from 'express';
-import path from 'path';
 import authRouter from './auth.route.js';
 import messageRouter from './messages.route.js';
 import userRouter from './user.route.js';
-// import arcjetProtection from '../middleware/arcjet.middleware.js';
+import arcjetProtection from '../middleware/arcjet.middleware.js';
 
 const router = Router();
 
-// router.use(arcjetProtection);
-router.get('/ping', (req: Request, res: Response) => {
-  console.log('ping')
-})
+router.use(arcjetProtection);
+
+// Fast test route – IMPORTANT: use valid HTTP status 200
+router.get('/ping', (_req: Request, res: Response) => {
+  return res.status(200).json({ message: 'pinged' });
+});
 
 router.use('/auth', authRouter);
-router.use('/users', userRouter)
+router.use('/users', userRouter);
 router.use('/messages', messageRouter);
 
 export default router;
